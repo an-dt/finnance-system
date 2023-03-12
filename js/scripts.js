e3cd1d2b75a813848b81c6e42c64ddab16c80348
an-dt <104084358+an-dt@users.noreply.github.com>
2023-03-12 22:17:30 +0700

    changes

--- js/scripts.js
+++ js/scripts.js
@@ -29,38 +29,61 @@ var addTransactionBtn = document.getElementById("addTransactionBtn");
 var addTransactionDetails = document.getElementById("addTransactionDetails");
 
 addTransactionBtn.addEventListener("click", function() {
+
   if (addTransactionDetails.style.display === "none") {
     addTransactionDetails.style.display = "block"; // Hiển thị phần tử
+    
+    document.addEventListener('mouseup', function(e) {
+      var container = document.getElementById('addTransactionDetails');
+      if (!container.contains(e.target)) {
+          container.style.display = 'none';
+      }
+  });
   } else {
     addTransactionDetails.style.display = "none"; // Ẩn phần tử đi
   }
+  
 });
+
 var addcategory = document.getElementById("addcategory");
 var addcategorydetails = document.getElementById("addcategorydetails");
-
 addcategory.addEventListener("click", function() {
   if (addcategorydetails.style.display === "none") {
     addcategorydetails.style.display = "block";
+    if( adddatedetails.style.display === "block"){
+      adddatedetails.style.display = "none";
+    }
   } else {
     addcategorydetails.style.display = "none";
   }
 });
+
+//kích vào thì hiện datetrans
 var adddate = document.getElementById("adddate");
 var adddatedetails = document.getElementById("adddatedetails");
 
 adddate.addEventListener("click", function() {
   if (adddatedetails.style.display === "none") {
     adddatedetails.style.display = "block";
-  } else {
+    if( addcategorydetails.style.display === "block"){
+      addcategorydetails.style.display = "none";
+    }
+  }
+   else {
     adddatedetails.style.display = "none";
   }
+  
 });
+
+
+
 var closeBtn = document.getElementById("closeBtn");
 var addTransactionDetails = document.getElementById("addTransactionDetails");
 
 closeBtn.addEventListener("click", function() {
   if (addTransactionDetails.style.display === "none") {
     addTransactionDetails.style.display = "block"; // Hiển thị phần tử
+    
   } else {
     addTransactionDetails.style.display = "none"; // Ẩn phần tử đi
   }
