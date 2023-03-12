68a0227cdf774863c8aa685d42792dd45ed962be
an-dt <104084358+an-dt@users.noreply.github.com>
2023-03-12 22:30:08 +0700

    changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,18 @@
-/*!
-    * Start Bootstrap - SB Admin v7.0.5 (https://startbootstrap.com/template/sb-admin)
-    * Copyright 2013-2022 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
-    */
-    // 
-// Scripts
-// 
-
 window.addEventListener('DOMContentLoaded', event => {
 
-    // Toggle the side navigation
-    const sidebarToggle = document.body.querySelector('#sidebarToggle');
-    if (sidebarToggle) {
-        // Uncomment Below to persist sidebar toggle between refreshes
-        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
-        //     document.body.classList.toggle('sb-sidenav-toggled');
-        // }
-        sidebarToggle.addEventListener('click', event => {
-            event.preventDefault();
-            document.body.classList.toggle('sb-sidenav-toggled');
-            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
-        });
-    }
+  // Toggle the side navigation
+  const sidebarToggle = document.body.querySelector('#sidebarToggle');
+  if (sidebarToggle) {
+      // Uncomment Below to persist sidebar toggle between refreshes
+      // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+      //     document.body.classList.toggle('sb-sidenav-toggled');
+      // }
+      sidebarToggle.addEventListener('click', event => {
+          event.preventDefault();
+          document.body.classList.toggle('sb-sidenav-toggled');
+          localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+      });
+  }
 
 });
 
@@ -30,32 +21,32 @@ var addTransactionDetails = document.getElementById("addTransactionDetails");
 
 addTransactionBtn.addEventListener("click", function() {
 
-  if (addTransactionDetails.style.display === "none") {
-    addTransactionDetails.style.display = "block"; // Hiển thị phần tử
-    
-    document.addEventListener('mouseup', function(e) {
-      var container = document.getElementById('addTransactionDetails');
-      if (!container.contains(e.target)) {
-          container.style.display = 'none';
-      }
-  });
-  } else {
-    addTransactionDetails.style.display = "none"; // Ẩn phần tử đi
-  }
+if (addTransactionDetails.style.display === "none") {
+  addTransactionDetails.style.display = "block"; // Hiển thị phần tử
   
+  document.addEventListener('mouseup', function(e) {
+    var container = document.getElementById('addTransactionDetails');
+    if (!container.contains(e.target)) {
+        container.style.display = 'none';
+    }
+});
+} else {
+  addTransactionDetails.style.display = "none"; // Ẩn phần tử đi
+}
+
 });
 
 var addcategory = document.getElementById("addcategory");
 var addcategorydetails = document.getElementById("addcategorydetails");
 addcategory.addEventListener("click", function() {
-  if (addcategorydetails.style.display === "none") {
-    addcategorydetails.style.display = "block";
-    if( adddatedetails.style.display === "block"){
-      adddatedetails.style.display = "none";
-    }
-  } else {
-    addcategorydetails.style.display = "none";
+if (addcategorydetails.style.display === "none") {
+  addcategorydetails.style.display = "block";
+  if( adddatedetails.style.display === "block"){
+    adddatedetails.style.display = "none";
   }
+} else {
+  addcategorydetails.style.display = "none";
+}
 });
 
 //kích vào thì hiện datetrans
@@ -63,16 +54,16 @@ var adddate = document.getElementById("adddate");
 var adddatedetails = document.getElementById("adddatedetails");
 
 adddate.addEventListener("click", function() {
-  if (adddatedetails.style.display === "none") {
-    adddatedetails.style.display = "block";
-    if( addcategorydetails.style.display === "block"){
-      addcategorydetails.style.display = "none";
-    }
+if (adddatedetails.style.display === "none") {
+  adddatedetails.style.display = "block";
+  if( addcategorydetails.style.display === "block"){
+    addcategorydetails.style.display = "none";
   }
-   else {
-    adddatedetails.style.display = "none";
-  }
-  
+}
+ else {
+  adddatedetails.style.display = "none";
+}
+
 });
 
 
@@ -81,12 +72,12 @@ var closeBtn = document.getElementById("closeBtn");
 var addTransactionDetails = document.getElementById("addTransactionDetails");
 
 closeBtn.addEventListener("click", function() {
-  if (addTransactionDetails.style.display === "none") {
-    addTransactionDetails.style.display = "block"; // Hiển thị phần tử
-    
-  } else {
-    addTransactionDetails.style.display = "none"; // Ẩn phần tử đi
-  }
+if (addTransactionDetails.style.display === "none") {
+  addTransactionDetails.style.display = "block"; // Hiển thị phần tử
+  
+} else {
+  addTransactionDetails.style.display = "none"; // Ẩn phần tử đi
+}
 });
 // var closeBtn = document.getElementById("closeBtn");
 //   var dashboard = document.getElementById("close");
